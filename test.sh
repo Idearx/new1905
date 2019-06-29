fd5f699f715690abce0151a1093a0d023176039e
Your Name <you@example.com>
2019-06-29 09:25:14 +0800

    Copy Test.sh

---
+++ test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# this is a test script.
+
+# author: yanyalun20190628 AT beijing.
+
+# Alarm: this script will bomeo.
+
+function usage {
+  echo "Usage: $0 username."
+  exit 1
+}
+
+if [ $# -lt 2 ]; then
+    usage;
+fi
+
+current_day=`date +%Y%m%d-%H%M`
+
+folder="cccdddeeefff"
+
+echo "folder:${folder}"
+echo "folder len: ${#folder}"
+echo "sub folder: ${folder:3:4}"
+
+echo "hello $1,$current_day"
+echo "param numbers: $#"
+echo "params: $*"
+echo "create folder"
+
+mkdir -p aaa/bbb/${folder}
+
+cd aaa/bbb/$folder
+
+pwd
+
+for i in `seq 5`; do 
+    mkdir -p 5; 
+done
