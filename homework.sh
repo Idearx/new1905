dfeb7ca4a4d2dabda4a6124cf03a1b086c1368c3
Your Name <you@example.com>
2019-07-03 08:09:55 +0800

    The shell for .gz

--- homework.sh
+++ homework.sh
@@ -16,17 +16,26 @@ gzname="$echo "$(find ./ -name '*.gz')""
 echo "start"
 
 echo "$gzname"
+
 len="${#gzname}"
+
 echo "$len"
+
 str="${gzname:3:($len-2)}"
+
 echo "$str"
+
 tar -xzvf "$str"
+
 mulu="${str:0:($len-10)}"
+
 # echo "$mulu"
 cd ./$mulu
+
 echo "hello!modeify!" >> README.rst
+
 cd ..
+
 tar -czvf new.tar.gz $mulu
+
 echo "end"
-
-
